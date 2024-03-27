a4b975965610e63360cc46391de289e51e189218
D1EE7P2U9 <108419163+D1EE7P2U9@users.noreply.github.com>
2024-03-27 15:01:22 +0530

    Create mongo_s3.py
    ingest data from mongodb to s3

---
+++ mongo_s3.py
@@ -0,0 +1,22 @@
+import pymongo
+import pandas as pd
+import numpy as np
+
+#mongo Creds
+Mongo_String = '<<your-mongo-connection-string>>'
+database = '<<DataBaseName>>'
+collection = '<<Mongo-collection-Name>>'
+ 
+mongo_client = pymongo.MongoClient(Mongo_String)
+db = mongo_client[database]
+collection = db[collection]
+
+
+cursor = collection.find()
+df = pd.DataFrame(list(cursor))
+
+S3_Key = '<<s3_path_key>>'
+Bucket_Name = '<<bucket_name>>'
+
+s3_path = f"s3://{Bucket_Name}/{S3_Key}/{collection}/"
+df.to_csv(s3_path, index=False)
